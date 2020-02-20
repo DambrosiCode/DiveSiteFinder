e884333545982c72008a4a105f1b28c37aaca69a
Matthew D'Ambrosio <mattd221@comcast.net>
2020-02-19 19:31:12 -0500

    cleaned input code

--- gui_Interface.py
+++ gui_Interface.py
@@ -30,10 +30,10 @@ font.setPointSize(FONT_SIZE)
 class HelloWindow(QMainWindow):    
     def __init__(self):        
         QMainWindow.__init__(self)
-        self.setStyleSheet("QLabel {font: "+str(FONT_FAM)+"pt "+FONT_STYLE+"}")
+        self.setStyleSheet("QLabel {font: "+str(FONT_SIZE)+"pt "+FONT_FAM+"}")
 
         self.setMinimumSize(QSize(win_h, win_w))    
-        self.setWindowTitle("Dive Site Finder 0.0.2") 
+        self.setWindowTitle("Dive Site Finder 0.0.3") 
         
         centralWidget = QWidget(self)          
         self.setCentralWidget(centralWidget)   
@@ -55,28 +55,16 @@ class HelloWindow(QMainWindow):
         pass
     
     def text_boxes(self):
+        #input values
+        values = ['Winter', 'Spring', 'Summer', 'Fall']
         #set text box inputs
-        self.summ_temp = QLineEdit('1', self)
-        self.spri_temp = QLineEdit('2', self)
-        self.fall_temp = QLineEdit('3', self)
-        self.wint_temp = QLineEdit('4', self)
-        
-        #each box is spaced from the first box at y+it's height (to prevent overlap)+space
-        self.summ_temp.move(tb_x, (tb_y+tb_h*0+tb_sp*0))        
-        self.summ_temp.resize(tb_w, tb_h)
-        self.summ_temp.setFont(font)
-        
-        self.spri_temp.move(tb_x, (tb_y+tb_h*1+tb_sp*1))
-        self.spri_temp.resize(tb_w, tb_h)
-        self.spri_temp.setFont(font)
-        
-        self.fall_temp.move(tb_x, (tb_y+tb_h*2+tb_sp*2))
-        self.fall_temp.resize(tb_w, tb_h)
-        self.fall_temp.setFont(font)
-        
-        self.wint_temp.move(tb_x, (tb_y+tb_h*3+tb_sp*3))
-        self.wint_temp.resize(tb_w, tb_h)
-        self.wint_temp.setFont(font)
+        self.box = []
+    
+        #each box is spaced from the first box at y+it's height (to prevent overlap)+space        
+        for i in range(4):
+            self.box.append(QLineEdit(values[i], self))
+            self.box[i].move(tb_x, (tb_y+tb_h*i+tb_sp*i))
+            self.box[i].setFont(font)
         
     def button(self):
         self.button = QPushButton('show text', self)
@@ -86,7 +74,8 @@ class HelloWindow(QMainWindow):
         self.show()
 
     def on_click(self):
-        textbox_value = self.textbox.text()
+        wint_val = self.box[0].text()
+        
         QMessageBox.question(self, 'Message - pythonspot.com', "You typed: " + textbox_value)
         self.textbox.setText("")
         
