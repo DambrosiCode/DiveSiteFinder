1b28f336be366ef04def513c53421996b4a5fbfc
Matthew D'Ambrosio <mattd221@comcast.net>
2020-03-05 19:48:22 -0500

    fixed bugs

--- near_neigh.py
+++ near_neigh.py
@@ -20,30 +20,26 @@ class DiveFinder():
     def dive_loc(self, dive_params=[], n_neighbors=5):
         
         #if temp is int IE there's an input it will calculate the NN for that season(s)
-        inpts = [i for i, temp in enumerate(dive_params) if type(temp)==int]
-        data = [temp for i, temp in enumerate(dive_params) if type(temp)==int]
-        #which seasons were selected   
-        params = (self.cats[inpts])
-        
-        print(params)
-        
-        '''#NN model
+        inpts = [i for i, temp in enumerate(dive_params) if type(temp)==int] #index of param
+        data = [temp for i, temp in enumerate(dive_params) if type(temp)==int] #actual param number
+        params = (self.cats[inpts]) #param name
+                
+        #NN model
         self.X = self.dive_sites_df[params].dropna()
         self.X = np.array(self.X).reshape(-1,len(params))
         self.neigh = NearestNeighbors()
         self.neigh.fit(self.X)
-
+        
         #calculate the nearest neighbors
         dist, ind = self.neigh.kneighbors([data], n_neighbors=n_neighbors)
         
-        near_neighs = self.dive_sites_df.dropna(subset=self.cats).iloc[ind[0]]
-        self.cats.insert(0,'Location')
-        self.cats.insert(1,'Contact')
-        return(near_neighs[self.cats])
-        #TODO: input temps should be an array'''
+        near_neighs = self.dive_sites_df.dropna(subset=params).iloc[ind[0]]
+        #self.params(0,'Location')
+        #self.params(1,'Contact')
+        return(near_neighs[params])
 
 dive = DiveFinder()
-dive.dive_loc(['Winter', 60,'Summer', 70])
+print(dive.dive_loc(['Winter', 60,'Summer', 70]))
 
 
 
