a11893fb2656b2930598930070f57624e3a87bab
Matthew D'Ambrosio <mattd221@comcast.net>
2020-03-13 15:26:14 -0400

    sep params

--- near_neigh.py
+++ near_neigh.py
@@ -17,28 +17,41 @@ class DiveFinder():
         
         self.cats = np.array(['Winter','Spring','Summer','Fall'], dtype=str)
 
-    def dive_loc(self, dive_params=[], n_neighbors=5):
+    def dive_loc(self, winter='Winter', spring='Spring', summer='Summer', fall='Fall', n_neighbors=5):
+        dive_params = dict() #{ParamName:ParamValue}
+        
+        
+        #if the parameter value is quantifiable than is will be considered in the NN algo
+        for i, param in enumerate([winter, spring, summer, fall]):
+            if isinstance(param, int):
+                dive_params[self.cats[i]] = param
+                
+        self.dive_sites_df = self.dive_sites_df[dive_params.keys()]
         
         #if temp is int IE there's an input it will calculate the NN for that season(s)
-        inpts = [i for i, temp in enumerate(dive_params) if type(temp)==int] #index of param
-        data = [temp for i, temp in enumerate(dive_params) if type(temp)==int] #actual param number
-        params = (self.cats[inpts]) #param name
-                
+        #inpts = [i for i, temp in enumerate(dive_params.values()) if type(temp)==int] #index of param
+        data = dive_params.values() #actual param number
+        params = dive_params.keys() #param name
+
         #NN model
         self.X = self.dive_sites_df[params].dropna()
+        
         self.X = np.array(self.X).reshape(-1,len(params))
+        
         self.neigh = NearestNeighbors()
         self.neigh.fit(self.X)
         
+        dist, ind = self.neigh.kneighbors([[65,65],[65,65]],n_neighbors=n_neighbors)
+        
         #calculate the nearest neighbors
-        dist, ind = self.neigh.kneighbors([data], n_neighbors=n_neighbors)
+        #dist, ind = self.neigh.kneighbors([60,100], n_neighbors=n_neighbors)
         
         near_neighs = self.dive_sites_df.dropna(subset=params).iloc[ind[0]]
-        params = np.append(['Location'], params)
+        #params = np.append(['Location'], params)
         return(near_neighs[params])
 
-#dive = DiveFinder()
-#print(dive.dive_loc(['Winter', 60,'Summer', 70]))
+dive = DiveFinder()
+print(dive.dive_loc(spring=60, summer=100))
 
 
 
